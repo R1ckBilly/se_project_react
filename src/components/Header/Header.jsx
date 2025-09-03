18d5b62a02e439594504e708a8350e5d7183bf65
Haniel <hanielgam1910@gmail.com>
2025-09-03 18:58:11 -0500

    Finished the Header set-up

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import "./Header.css";
+import logo from "../../images/wtwr_logo.svg";
+import avatar from "../../images/avatar_logo.svg";
+
+function Header() {
+  const now = new Date();
+    const dateStr = now.toLocaleDateString("default", {
+      month: "long",
+      day: "numeric",
+    });
+
+  return (
+    <header className="header">
+      <img src={logo} alt="WTWR logo" className="header__logo" />
+      <p className="header__place">
+        <time className="header__datetime" dateTime={now}>
+          {dateStr}
+        </time>
+        , Caracas
+      </p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <p className="header__username">Haniel Alvarez</p>
+      <img
+        src={avatar}
+        alt="Haniel Alvarez's avatar"
+        className="header__avatar"
+      />
+    </header>
+  );
+}
+
+export default Header;
